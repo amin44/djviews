f110957d232ae8125d8d4926db89a309759e2b57
mehrab_nourani <mehrab_262@yahoo.com>
2017-03-12 00:11:27 +0330

    finish 6 session

---
+++ Desktop/djviews/views/views/myviews.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+
+def homepage(request):
+    print(dir(request))
+    return HttpResponse("<!DOCTYPE html><html><head><h1 style=text-align:center>hello django learner</h1></head><body style=background-color:orange></body></html>")
+
+def home(request):
+    response=HttpResponse()
+    response.write("this is from respond")
+    return (response)
--- Desktop/djviews/views/views/urls.py
+++ Desktop/djviews/views/views/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from .myviews import homepage
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', homepage, name='homepage'),
 ]
