29ecf9ae7886305676ca5069c2069c4436f21d4c
mehrab_nourani <mehrab_262@yahoo.com>
2017-03-15 17:41:35 +0330

    finish session 9

--- Desktop/djviews/views/web/views.py
+++ Desktop/djviews/views/web/views.py
@@ -6,4 +6,8 @@ from .models import PostModel
 def post_model_list_view(request):
     qs=PostModel.objects.all()
     print(qs)
-    return HttpResponse("we make def and use web->urls and go to views->urls and map web urls in to /web/ and now we see all views of web")
+    # return HttpResponse("we make def and use web->urls and go to views->urls and map web urls in to /web/ and now we see all views of web")
+    template_path="web/list_view.html"
+    context_dictioanry={"query_list":qs}
+    # but dont show list.title when use query_list???
+    return render(request,template_path,context_dictioanry)
