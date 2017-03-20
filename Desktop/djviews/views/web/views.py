2d00f44fd298ff275b3fa7272a3fdef4b6959c88
mehrab_nourani <mehrab_262@yahoo.com>
2017-03-20 17:50:06 +0330

    finish session 11

--- Desktop/djviews/views/web/views.py
+++ Desktop/djviews/views/web/views.py
@@ -1,13 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse , Http404
 from .models import PostModel
 # Create your views here.
+@login_required(login_url='/login')
+
+def post_model_detail_view(request,id=None):
+    # qs=PostModel.objects.get(id=120)
+    # if not qs.exists():
+    #     raise Http404
+    # else:
+    #     obj=qs.first()
+    obj=PostModel.objects.get(id=id)
+    template_path = "web/detail_view.html"
+    context_dictioanry={"object":obj}
+    return render(request,template_path,context_dictioanry)
 
 def post_model_list_view(request):
+    # qs=PostModel.objects.all()
+    # # return HttpResponse("we make def and use web->urls and go to views->urls and map web urls in to /web/ and now we see all views of web")
+    # template_path="web/list_view.html"
+    # context_dictioanry={"query_list":qs}
+    # # but dont show list.title when use query_list???
+    # return render(request,template_path,context_dictioanry)
+
+    print(request.user)
     qs=PostModel.objects.all()
-    print(qs)
-    # return HttpResponse("we make def and use web->urls and go to views->urls and map web urls in to /web/ and now we see all views of web")
-    template_path="web/list_view.html"
-    context_dictioanry={"query_list":qs}
-    # but dont show list.title when use query_list???
-    return render(request,template_path,context_dictioanry)
+    context={"object_list":qs}
+    if request.user.is_authenticated():
+        template="web/list_view.html"
+    else:
+        template="web/list_view_public.html"
+    return render(request,template,context)
